28a29943a5ccaba2e0ee0a6e4d4456ff204832e2
xuerongNanopay <xuerong@nanopay.net>
2023-06-24 19:33:17 -0400

    add mongoose

--- backend/src/boot.ts
+++ backend/src/boot.ts
@@ -1,7 +1,19 @@
 // Preload config
 import config from 'config';
+import mongoose from 'mongoose';
+
+const bootMongoose = async () => {
+  try {
+    const url = config.get("MONGO_DB.URL") as string;
+    mongoose.connect(url);
+    console.info("Mongoose Initial Successfully!");
+  } catch ( error ) {
+    throw error;
+  }
+}
+
 const runBoot = async () => {
-  console.log('runBoot: ', config.get("Title"));
+  await bootMongoose();
 }
 
 export default runBoot;
