6c827666976513299714e98a15bec3ced044c361
xuerongNanopay <xuerong@nanopay.net>
2023-06-25 19:14:13 -0400

    add reference

--- backend/src/models/account.ts
+++ backend/src/models/account.ts
@@ -1,17 +1,36 @@
-interface IAccount {
+import { AddressData } from "./basic-type";
+
+export interface IAccount<ID, OWER_ID> {
   type: string;
-  id: number,
+  id: ID,
   name: string;
   domination: string;
   description?: string;
   isDefault?: boolean;
-  owner: object;
+  owner: OWER_ID;
   lifeCycleState: string;
   transferIn: boolean;
   transferOut: boolean;
 
   createdAt: Date;
   updatedAt: Date;
-  createBy: object;
-  updateBy: object;
+  createBy: OWER_ID;
+  updateBy: OWER_ID;
 }
+
+export interface IBankAcount<ID, OWER_ID> extends IAccount<ID, OWER_ID> {
+  accountNumber?: string;
+  iban?: string;
+  institutionNumber: string;
+  branchId: string;
+  instruction: string;
+  bankAddress: AddressData;
+  country: string;
+  flagImage: string;
+  status: string;
+  verificationAttempts: string;
+  verifyBy: any; // Plaid or Flinks or void check
+}
+
+export interface ICABankAcount<ID, OWER_ID> extends IAccount<ID, OWER_ID> {}
+export interface IPKBankAcount<ID, OWER_ID> extends IAccount<ID, OWER_ID> {}
