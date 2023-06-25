2c8d81743857c027d13312f9f890690f3f886dc2
xuerongNanopay <xuerong@nanopay.net>
2023-06-25 18:56:58 -0400

    add account interface

--- backend/src/models/account.ts
+++ backend/src/models/account.ts
@@ -1,4 +1,17 @@
-interface Account {
+interface IAccount {
+  type: string;
   id: number,
-  
+  name: string;
+  domination: string;
+  description?: string;
+  isDefault?: boolean;
+  owner: object;
+  lifeCycleState: string;
+  transferIn: boolean;
+  transferOut: boolean;
+
+  createdAt: Date;
+  updatedAt: Date;
+  createBy: object;
+  updateBy: object;
 }
