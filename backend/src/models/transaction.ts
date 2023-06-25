74e0c96d2ebf040c1fccbf77ef99e4626fa2f84c
xuerongNanopay <xuerong@nanopay.net>
2023-06-25 19:46:31 -0400

    add transaction interface

---
+++ backend/src/models/transaction.ts
@@ -0,0 +1,29 @@
+export interface IFee {
+  name: string;
+  amount: number;
+  currency: string;
+}
+
+export interface ITransfer<F, T> {
+  type: string;
+  from: F,
+  to: T,
+  fromAmount: number;
+  toAmount: number;
+  fromCurrency: string;
+  toCurrency: string;
+  fees: IFee[] | null
+}
+
+export interface ITransaction<S_ID, D_ID> {
+  type: string;
+  source: S_ID;
+  destination: D_ID;
+  sourceAmount: number;
+  destinationAmount: number;
+  sourceCurrency: string;
+  destinationCurrency: string;
+  transfers: ITransfer<any, any>[];
+  totalExpense: number;
+  totalFee: number; // grab data from transfer.
+}
