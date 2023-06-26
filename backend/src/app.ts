d62b50f264e99fa17a5801dcfc812ac448a02325
xuerongNanopay <xuerong@nanopay.net>
2023-06-25 22:09:36 -0400

    express server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,16 @@ console.log('Payment App start....')
 
 // import runBoot from "boot";
 import runBoot from "./boot"
+import config from 'config';
+
 let a = await runBoot();
 
-import { Express } from "express";
+import express from "express";
+import { BodyParser } from "body-parser";
+
+const httpServer = express();
+
+const httpPort = config.get('SERVER.PORT') as number | 3030;
+httpServer.listen(httpPort, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${httpPort}`);
+});
