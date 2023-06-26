83543148bd58636e81f0d173389cd53e9cb12a51
xuerongNanopay <xuerong@nanopay.net>
2023-06-26 18:57:34 -0400

    TODO: resolve path hell

--- backend/src/boot.ts
+++ backend/src/boot.ts
@@ -2,12 +2,13 @@
 import config from 'config';
 import mongoose from 'mongoose';
 import testInputUser from './models/user-test'
+import logger from './utils/logger';
 
 const bootMongoose = async () => {
   try {
     const url = config.get("MONGO_DB.URL") as string;
     mongoose.connect(url);
-    console.info("Mongoose Initial Successfully!");
+    logger.info("Mongoose Initial Successfully!");
     await testInputUser();
   } catch ( error ) {
     throw error;
